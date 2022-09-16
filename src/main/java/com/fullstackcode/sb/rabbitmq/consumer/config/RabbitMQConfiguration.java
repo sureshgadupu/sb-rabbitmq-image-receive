6c8c183c471e03460b22f9faf32f909be268b504
sureshgadupu <sureshgkhyd@gmail.com>
2022-09-16 18:16:54 +1200

    first commit

---
+++ src/main/java/com/fullstackcode/sb/rabbitmq/consumer/config/RabbitMQConfiguration.java
@@ -0,0 +1,34 @@
+package com.fullstackcode.sb.rabbitmq.consumer.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fullstackcode.sb.rabbitmq.consumer.model.Event;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+
+    @Bean
+    MessageConverter messageConverter() {
+        Jackson2JsonMessageConverter jsonConverter = new Jackson2JsonMessageConverter();
+        return jsonConverter;
+    }
+
+
+
+}
